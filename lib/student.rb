16125c8150bdcaa8df46e63ee2eba27e98755841
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-09 11:36:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,11 +24,7 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-      CREATE TABLE students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade INTEGER
-      );
+      DROP TABLE students;
     SQL
     DB[:conn].execute(sql)
   end
