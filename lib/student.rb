3b602e42ad196376ef310b3ba9e96b8989d4e0ec
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-09 11:48:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,9 +30,9 @@ class Student
   end
 
   def save
-  
+
       sql = <<-SQL
-        INSERT INTO students (name, grade) 
+        INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
 
